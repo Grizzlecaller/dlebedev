a21c900ff9c1ae3c7eb1e09ab114e4e58ff6c433
Grizzlecaller <dylebedev.other@gmail.com>
2018-03-08 01:13:51 +0300

    test

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,17 +1,16 @@
 package ru.job4j;
-
 /**
  * Class calculate 1.1 Hello World
  * @author dlebedev
  * @since 06.03.2018
  */
-
-/**
- * Method main
- * @param args -args
- * @return print Hello World
- */
 public class Calculate {
+
+	/**
+	 * Method main
+	 * @param args -args
+	 * @return print Hello World
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
