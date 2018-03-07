b61a7cc82861f2c5d14aadfce9f64acb8a0f90eb
Grizzlecaller <dylebedev.other@gmail.com>
2018-03-08 01:03:25 +0300

    test

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,14 +1,14 @@
 
 package ru.job4j;
-import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+		import org.junit.Test;
+		import java.io.ByteArrayOutputStream;
+		import java.io.PrintStream;
+		import static org.hamcrest.core.Is.is;
+		import static org.junit.Assert.assertThat;
 
 /**
  * Test
- * 
+ *
  * @author Dmitry Lebedev (dylebedev@gmail.com)
  * @version $Id$
  * @since 06.03.2018
