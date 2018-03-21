0e4fa83db53cb32deb4ee4bede0fee0c6b5b1bb3
Grizzlecaller <dylebedev.other@gmail.com>
2018-03-21 19:33:12 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+/**
+ * Class loop 0.1
+ * @author Dmitry Lebedev
+ * @since 21.03.2018
+ */
+
+public class Counter {
+    public int add(int start, int finish) {
+        int alfa = 0;
+        for(int i=start;i<=finish;i++){
+            if (i % 2 == 0) {
+                /*
+                 *alfa принимает значение i, когда i кратно 2
+                 */
+                alfa += i;
+            }
+        }
+        return alfa;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Dmitry Lebedev
+ * @since 21.03.2018
+ */
+
+public class CounterTest{
+    @Test
+    public void whenFirstLessSecond() {
+        Counter summ = new Counter();
+        int result = summ.add(0, 10);
+        assertThat(result, is(30));
+    }
+}
