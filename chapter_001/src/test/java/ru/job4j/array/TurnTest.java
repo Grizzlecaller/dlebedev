97bbcc6c9e897536b980c91107622d932c0f131a
Grizzlecaller <dylebedev.other@gmail.com>
2018-04-04 22:36:05 +0300

    4.3. Построить шахматную доску в псевдографике. [#13559] Исправление ошибок 2

--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -21,6 +21,10 @@ public class TurnTest {
 
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
-        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+        Turn turner = new Turn();
+        int[] input = new int[] {2, 6, 1,4 };
+        int[] result = turner.turn(input);
+        int[] expect = new int[] {4, 1, 6, 2 };
+        assertThat(result, is(expect));
     }
 }
