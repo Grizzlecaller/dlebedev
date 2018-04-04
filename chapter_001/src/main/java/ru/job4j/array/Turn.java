51a9fcb0c7c5c7fd33761d38461e7a88cdee4243
Grizzlecaller <dylebedev.other@gmail.com>
2018-04-04 17:57:28 +0300

    5.2. Перевернуть массив. [#4441] Очередная попытка осилить переворачивание массива.

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * Class Paint
+ * @author Dmitry Lebedev
+ * @since 03.04.2018
+ */
+
+public class Turn {
+    int xedni;
+    int a = 0;
+    public int[] turn(int[] array) {
+        for (int index = 0; index < array.length; index++) {
+            array[index] = index;
+
+        }
+        /*public int[] back ( int[] array){
+            for (xedni = array.length; xedni >= array.length; xedni--) {
+                array[xedni] = 1;
+
+
+            }
+            return array;
+        }*/return array;
+    }
+}
+
