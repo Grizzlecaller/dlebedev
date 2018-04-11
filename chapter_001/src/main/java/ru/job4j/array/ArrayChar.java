991fe6c9fa89a8122e884bad68dd4c0e37407896
Grizzlecaller <dylebedev.other@gmail.com>
2018-04-11 10:02:19 +0300

    5.5. Слова начинается с ... [#41585] Рабочий вариант.

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -26,9 +26,7 @@ public class ArrayChar {
         char[] value = prefix.toCharArray();
         // проверить. что массив data имеет первые элементы одинаковые с value
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == (data[i])) {
-                return true;
-            } else {
+            if (value[i] != (data[i])) {
                 return false;
             }
         }
