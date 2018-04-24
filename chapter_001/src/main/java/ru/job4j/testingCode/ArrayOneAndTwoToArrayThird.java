e561d35374ea806bcf8143fa77e4894f4f91d597
Grizzlecaller <dylebedev.other@gmail.com>
2018-04-24 12:11:12 +0300

    Задание от Петра Арсентьева. Готовый вариант

---
+++ chapter_001/src/main/java/ru/job4j/testingCode/ArrayOneAndTwoToArrayThird.java
@@ -0,0 +1,37 @@
+package ru.job4j.testingCode;
+
+import java.lang.reflect.Array;
+
+/**
+ * Class BubbleSort
+ * @author Dmitry Lebedev
+ * @since 23.04.2018
+ */
+
+/*
+    Из двух отсортированных массивов перенести элементы в третий массив блез сортировки.
+ */
+
+public class ArrayOneAndTwoToArrayThird {
+    public int[] Fin(int[] array1, int[] array2) {
+        //Указываем длинну конечного массива равную сумме длин array1 и array2
+        int[] arrayFin = new int[array1.length + array2.length];
+        /*
+         * Двумя циклами заполняем массив arrayFin
+         */
+            /*
+             *  Заполняем массив arrayFin из array1
+             */
+            for (int i = 0; i < array1.length; i++) {
+                arrayFin[i] = array1[i];
+            }
+            /*
+             *  Заполняем массив arrayFin из array2
+             */
+            for (int j = 0; j < array2.length; j++) {
+                //Заполнение arrayFin начинается с конца массива array1.
+                arrayFin[array1.length + j] = array2[j];
+            }
+        return arrayFin;
+    }
+}
--- chapter_001/src/test/java/ru/job4j/testingCodeTest/ArrayOneAndTwoToArrayThirdTest.java
+++ chapter_001/src/test/java/ru/job4j/testingCodeTest/ArrayOneAndTwoToArrayThirdTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.array;
+package ru.job4j.testingCodeTest;
 
 /**
  * @author Dmitry Lebedev
@@ -6,18 +6,19 @@ package ru.job4j.array;
  */
 
 import org.junit.Test;
+import ru.job4j.testingCode.ArrayOneAndTwoToArrayThird;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ArrayOneAndTwoToArrayThirdTest {
     @Test
-    public void WhenArrayOneAndTwoToArrayThird() {
+    public void WhenArrayOneAndTwoThenArrayThird() {
         ArrayOneAndTwoToArrayThird ArrayOne = new ArrayOneAndTwoToArrayThird();
         int[] inputOne = new int[] {1, 2, 3, 4, 5};
         int[] inputTwo = new int[] {10, 20, 30, 40, 50};
-        int[] resultOne = ArrayOne.Try(inputOne);
-        //int[] resultTwo = ArrayOne.Try2(inputTwo);
-        int[] expect = new int[] {1, 2, 3, 4, 5, 11, 12, 13, 14, 15};
+        int[] resultOne = ArrayOne.Fin(inputOne, inputTwo);
+        int[] expect = new int[] {1, 2, 3, 4, 5, 10, 20, 30, 40, 50};
         assertThat(resultOne, is(expect));
     }
 }
