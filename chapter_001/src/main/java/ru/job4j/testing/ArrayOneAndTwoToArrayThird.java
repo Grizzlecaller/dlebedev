5bf4eb9fb2a3f11189e973b0d620da7bc1f057b3
Grizzlecaller <dylebedev.other@gmail.com>
2018-04-26 15:04:36 +0300

    Задание от Петра Арсентьева. Убрал комментарии в коде.

--- chapter_001/src/main/java/ru/job4j/testing/ArrayOneAndTwoToArrayThird.java
+++ chapter_001/src/main/java/ru/job4j/testing/ArrayOneAndTwoToArrayThird.java
@@ -6,28 +6,13 @@ package ru.job4j.testing;
  * @since 23.04.2018
  */
 
-/*
-    Из двух отсортированных массивов перенести элементы в третий массив блез сортировки.
- */
-
 public class ArrayOneAndTwoToArrayThird {
     public int[] fin(int[] array1, int[] array2) {
-        //Указываем длинну конечного массива равную сумме длин array1 и array2
         int[] arrayFin = new int[array1.length + array2.length];
-        /*
-         * Двумя циклами заполняем массив arrayFin
-         */
-            /*
-             * @param Заполняем массив arrayFin из array1
-             */
             for (int i = 0; i < array1.length; i++) {
                 arrayFin[i] = array1[i];
             }
-            /*
-             *  Заполняем массив arrayFin из array2
-             */
             for (int j = 0; j < array2.length; j++) {
-                //Заполнение arrayFin начинается с конца массива array1.
                 arrayFin[array1.length + j] = array2[j];
             }
         return arrayFin;
