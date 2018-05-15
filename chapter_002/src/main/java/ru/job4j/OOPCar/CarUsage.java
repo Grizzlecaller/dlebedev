08e39fd1f40c3afd8737773445d99997d4231404
Grizzlecaller <dylebedev.other@gmail.com>
2018-05-15 20:12:35 +0300

    Car lesson.

---
+++ chapter_002/src/main/java/ru/job4j/OOPCar/CarUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.OOPCar;
+
+public class CarUsage {
+    public static void main(String[] args) {
+        Car audi = new Car();
+        boolean driving = audi.canDrive();
+        System.out.println("Can you drive?:" + driving);
+        System.out.println("i`m going to a gas station.");
+        int gas = 25;
+        audi.fill(gas);
+        driving = audi.canDrive();
+        System.out.println("Can you drive now? :" + driving);
+        System.out.println("Yes");
+        System.out.println("10 km");
+        int distance = 10;
+        audi.drive(distance);
+        audi.gasInfo();
+    }
+}
