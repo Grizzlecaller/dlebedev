c35ceaf41ac7bf5e23355fbf94e495bac17f8fe9
Grizzlecaller <dylebedev.other@gmail.com>
2018-05-20 15:34:29 +0300

    Реализовать класс Tracker [#396] Продолжаем. replace strong

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -100,7 +100,7 @@ public class Tracker {
     void replace(String id, Item item) {
         for (Item findId : items) {
             if (findId != null && findId.getId().equals(id)) {
-               item.setId(findId.getId());
+
             }
         }
     }
