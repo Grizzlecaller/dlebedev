3d518b484ba9ac850c5b20edf102e0d1b9e04423
Grizzlecaller <mailto:dylebedev.other@gmail.com>
2018-08-30 21:58:11 +0300

    3. if-else-throw[#68898] done.

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -27,25 +27,6 @@ public class ConsoleInput implements Input {
 
     public int ask(String question, List<Integer> range) {
         System.out.println(question);
-        /*boolean b = true;
-        do {
-            try { //заварачиваем в трай если вдруг пользователь решит ввести строку а не цифру
-                key = Integer.parseInt(scanner.nextLine()); //считываем данные
-                for (Integer r : range) { //запускаем цикл по массиву
-                    if (key == r) { //если нашли совпадение то данный ключ существует
-                        b = false; //меняем флаг чтоб выйти из цикла
-                        break;
-                    }
-                }
-                if (b) { // если не нашли пишем об этом и цикл прокрутится заново
-                    System.out.println("Введите данные в диапазоне меню");
-                }
-            } catch (Exception ex) { //если возикло исключение пишем о некоректном вводе и т.к флаг не изменился
-                System.out.println("Введите корректные данные. "); //то цикл прокрутится еще раз
-            }
-        } while (b);
-        return key;*/
-
         int key = Integer.valueOf(this.ask(question));
         boolean exist = false;
         for (int value : range) {
@@ -54,11 +35,10 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return  key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("out of menu range");
         }
+        return key;
     }
     public Boolean checkMenuNumber() {
         boolean b = false;
--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -25,33 +25,9 @@ public class ValidateInput implements Input {
     }
 
     public int ask(String question, List<Integer> range) {
-        return -1;
-    }
-    /*public int ask(String question, List<Integer> range) {
-        class checkMenuNumber {
-            boolean valid1 = false;
-            int value = -1;
-            if (this.checkMenuNumber()) {
-
-            }
-        }
-        return -1;
-    }*/
-
-private final List<Consumer<ConsoleInput>> valid = Arrays.asList(
-        item -> {
-            if (!this.checkMenuNumber(item)) {
-                throw new MenuOutException("enter key from menu");
-            }
-        }
-);
-
-// checkMenuNumber(MenuOutException moe)
-// checkNotLetter(NumberFormatException nfe)
-
-    /*boolean invalid = true;
+        boolean invalid = true;
         int value = -1;
-        do {
+        do    {
             try {
                 value = this.input.ask(question, range);
                 invalid = false;
@@ -60,8 +36,7 @@ private final List<Consumer<ConsoleInput>> valid = Arrays.asList(
             } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again.");
             }
-        } while (invalid);
+        } while(invalid);
         return value;
-*/
-
+    }
 }
