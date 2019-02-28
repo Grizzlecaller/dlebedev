bb6b8bd623dcef72d8bc5914c5daefb4b78181e4
Grizzlecaller <dylebedev.other@gmail.com>
2019-02-28 22:29:37 +0300

    2. Очередь с приоритетом на LinkedList [#41670]
    продолжение.

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -11,15 +11,6 @@ import java.util.LinkedList;
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
-    public LinkedList<Task> test() {
-        LinkedList<Task> result = new LinkedList<Task>();
-        for (Task tmp : tasks) {
-                result.add(tmp.getPriority(), tmp);
-        }
-        return result;
-    }
-
-
     /**
      * Метод должен вставлять в нужную позицию элемент.
      * Позиция определять по полю приоритет.
@@ -27,13 +18,18 @@ public class PriorityQueue {
      * @param task задача
      */
 
-
     public void put(Task task) {
         //TODO добавить вставку в связанный список.
-
-        this.tasks.add(task);
+        /**
+         * тут происходит вставка в список, на нулевой индекс
+         * при этом каждый раз инициализируется метод put
+         * следовательно надо каким-то образом считать количество инициализаций
+         * и что?
+         * и уже в готовом связном списке сортировать с помощью Comparator
+         *
+         */
+        this.tasks.add(task.getPriority(), task);
     }
-
 
     public Task take() {
         return this.tasks.poll();
