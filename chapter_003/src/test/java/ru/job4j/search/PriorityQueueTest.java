bbc1c3e1a1b682e3812118f27dc8ccb36f60465d
Grizzlecaller <dylebedev.other@gmail.com>
2019-03-24 22:35:32 +0300

    2. Очередь с приоритетом на LinkedList[#117472]
    как обычно всё с нуля переделать.

--- chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -14,36 +14,21 @@ public class PriorityQueueTest {
     @Test
     public void whenHigherPriority() {
         PriorityQueue queue = new PriorityQueue();
-        /*queue.put(new Task("five", 5));
-        queue.put(new Task("six", 6));
-        queue.put(new Task("one", 1));
-        queue.put(new Task("two", 2));
-        queue.put(new Task("sewen", 7));
-        queue.put(new Task("ten", 10));
-        queue.put(new Task("three", 3));
-        queue.put(new Task("four", 4));
-*/
+
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
         queue.put(new Task("four", 4));
-        queue.put(new Task("ten", 10));
-        Task result = queue.take();
+        //queue.put(new Task("ten", 10));
+        //Task result = queue.take();
         //assertThat(result.getDesc(), is("urgent"));
 
-        System.out.println(queue.tasks.get(0).getDesc());
-        System.out.println(queue.tasks.get(1).getDesc());
-        System.out.println(queue.tasks.get(2).getDesc());
-        System.out.println(queue.tasks.get(3).getDesc());
-        //System.out.println(queue.tasks.get(2).getDesc());
-        /*System.out.println(queue.tasks.get(3).getDesc());
-        System.out.println(queue.tasks.get(4).getDesc());
-        System.out.println(queue.tasks.get(5).getDesc());
-        System.out.println(queue.tasks.get(6).getDesc());
-        System.out.println(queue.tasks.get(7).getDesc());
-*/
 
+        for (int i = 0; i < queue.tasks.size(); i++) {
+            System.out.println(queue.tasks.get(i).getDesc());
+        }
+        System.out.println("---------------------");
+        System.out.println("Размер связного списка = " + queue.tasks.size());
 
-        System.out.println(queue.tasks.size());
     }
 }
