734d631fe9fcd9effe879267ac6963fbd533a946
Grizzlecaller <dylebedev.other@gmail.com>
2019-04-15 13:09:05 +0300

    4. Конвертация двумерного массива в ArrayList [#48580]
    Начало.

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,17 @@
+package ru.job4j.list;
+
+/**
+ * Class ConvertMatrix2List
+ * @author Dmitry Lebedev
+ * @since 15.04.2019
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertMatrix2List {
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        return list;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.list;
+
+/**
+ * Class ConvertMatrix2List
+ * @author Dmitry Lebedev
+ * @since 15.04.2019
+ */
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertMatrix2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
