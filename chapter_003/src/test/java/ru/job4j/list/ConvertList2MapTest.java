c3f2d6ac10af83e9bd1c890e5d0deed713c89329
Grizzlecaller <dylebedev.other@gmail.com>
2019-05-15 19:42:48 +0300

    2. Написать программу преобразования List в Map. [#10093]
    Продолжение.

--- chapter_003/src/test/java/ru/job4j/list/ConvertList2MapTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2MapTest.java
@@ -6,20 +6,26 @@ package ru.job4j.list;
  * @since 26.04.2019
  */
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class ConvertList2MapTest {
 
     @Test
     public void something() {
-        ConvertList2Map map = new ConvertList2Map();
+        ConvertList2Map test = new ConvertList2Map();
 
+        final List<ConvertList2Map.User> actual = Arrays.asList(
+                new ConvertList2Map.User("test", "test", 0),
+                new ConvertList2Map.User("test1", "test1", 1),
+                new ConvertList2Map.User("test2", "test2", 2)
+        );
+
+        Assert.assertEquals(expected, actual);
     }
 }
