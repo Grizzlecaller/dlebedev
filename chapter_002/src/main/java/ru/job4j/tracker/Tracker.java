108e8f7cf6cd9912c84797f76e7a0ecd6bf5a07f
Grizzlecaller <dylebedev.other@gmail.com>
2019-05-30 20:40:01 +0300

    3. Изменить программу Tracker из 2-го модуля [#10039]
    Продолжение.

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -16,7 +16,7 @@ public class Tracker {
     /**
      * Массив для хранение заявок.
      */
-    private List<Item> items = new ArrayList<Item>();
+    private ArrayList<Item> items = new ArrayList<Item>();
 
     /**
      * Указатель ячейки для новой заявки.
@@ -34,6 +34,8 @@ public class Tracker {
         return item;
     }
     */
+
+    //NEW
     public Item add(Item item) {
         item.setId(this.generateId());
         this.items.add(item);
@@ -62,8 +64,10 @@ public class Tracker {
         return result;
     }
     */
-    public List<Item> findById(String id) {
-        List<Item> result = new ArrayList<Item>();
+
+    //NEW
+    public ArrayList<Item> findById(String id) {
+        ArrayList<Item> result = new ArrayList<Item>();
         for (Item item : items) {
             if (item != null && item.getId().equals(id)) {
                 result.add(item);
@@ -83,14 +87,17 @@ public class Tracker {
         return result;
     }
      */
-    public List<Item> getAll() {
-        List<Item> result = new ArrayList<Item>();
+
+    //NEW
+    public ArrayList<Item> getAll() {
+        ArrayList<Item> result = new ArrayList<Item>();
         for (Item item : items) {
             result.add(item);
         }
         return result;
     }
 
+    /**
     public Item[] findAll() {
 
         int index = 0;
@@ -104,6 +111,12 @@ public class Tracker {
         }
         return Arrays.copyOf(items, position);
     }
+     */
+
+    //NEW
+    public List<Item> findAll() {
+        return items;
+    }
 
     /**
      * @param key принимает значение которое сравнивается с каким-либо параметром ячейки массива items.
@@ -111,6 +124,7 @@ public class Tracker {
      *
      */
 
+    /**
     public Item[] findByName(String key) {
         int index = 0;
         Item[] result = new Item[this.position];
@@ -120,7 +134,21 @@ public class Tracker {
             }
         } return Arrays.copyOf(result, index);
     }
+     */
 
+    //NEW
+    public ArrayList<Item> findByName(String key) {
+        ArrayList<Item> result = new ArrayList<Item>();
+        for (Item findName : items) {
+            if (findName != null && findName.getName().equals(key)) {
+                result.add(findName);
+            }
+        }
+        return result;
+    }
+
+
+    /**
     public boolean delete(String id) {
         boolean k = false;
         for (int index = 0; index != this.position; index++) {
@@ -132,8 +160,21 @@ public class Tracker {
             }
         } return k;
     }
+     */
+
+    // NEW!
+    public boolean delete(String id) {
+        boolean k = false;
+        if (items.removeIf(item -> item.getId().equals(id))) {
+            k = true;
+            items.trimToSize();
+        }
+        return k;
+    }
 
 
+
+    /**
     public boolean replace(String id, Item item) {
         boolean k = false;
         int index = 0;
@@ -146,6 +187,20 @@ public class Tracker {
                     break;
                 }
                 index++;
+            }
+        }
+        return k;
+    }
+     */
+
+    //NEW
+    public boolean replace(String id, Item item) {
+        boolean k = false;
+        for (Item findId : items) {
+            if (findId != null && findId.getId().equals(id)) {
+                int index = items.indexOf(findId);
+                item.setId(id);
+                items.set(index, item);
             }
         }
         return k;
