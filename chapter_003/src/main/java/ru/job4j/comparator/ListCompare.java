93121513020997eef079ad9a163f968d57d7a617
Grizzlecaller <dylebedev.other@gmail.com>
2019-06-30 23:22:59 +0300

    3. Компаратор для строк. [#35008]
    Решение.

--- chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -6,6 +6,24 @@ import java.util.Comparator;
 public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        return 0;
+        int result = 0;
+        int i = 0;
+        int minSize = Math.min(left.length(),right.length());
+        do {
+            if(left.charAt(i) < right.charAt(i)) {
+                result = -1;
+                i++;
+                break;
+            } else if(left.charAt(i) > right.charAt(i)) {
+                result = 1;
+                i++;
+                break;
+            }
+            i++;
+        } while (i != minSize);
+        if (result == 0) {
+            result = Integer.compare(left.length(), right.length());
+        }
+        return result;
     }
 }
