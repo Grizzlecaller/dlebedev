3b940d37019d5bd19461ca39e511383827fad9e9
Grizzlecaller <dylebedev.other@gmail.com>
2019-07-10 16:45:04 +0300

    5.2 Банковские переводы. [#10038]
    Начало.

---
+++ chapter_003/src/main/java/ru/job4j/bankTransit/Account.java
@@ -0,0 +1,39 @@
+package ru.job4j.bankTransit;
+
+/**
+ * Class Account
+ * @author Dmitry Lebedev
+ * @since 10.07.2019
+ */
+
+public class Account {
+    private double value;
+    private String requisites;
+
+    public Account(int value, String requisites) {
+        this.value = value;
+        this.requisites = requisites;
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public String getRequisites() {
+        return this.requisites;
+    }
+
+    public boolean transfer(Account destination, double amount) {
+        boolean result = false;
+        if (amount > 0 && amount < this.value && destination != null) {
+            this.value -= amount;
+            destination.value += amount;
+            result = true;
+        }
+        return result;
+    }
+
+    public String toString() {
+        return "Account{" + "values=" + value + ", reqs='" + requisites + "\\" + "}";
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/bankTransit/User.java
@@ -0,0 +1,47 @@
+package ru.job4j.bankTransit;
+
+import ru.job4j.list.ConvertList2Map;
+
+import java.util.Objects;
+
+/**
+ * Class User
+ * @author Dmitry Lebedev
+ * @since 10.07.2019
+ */
+
+public class User {
+    private final String name;
+    private final int passport;
+
+    public User (String name, int passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPassport() {
+        return passport;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(name, user.name)
+                && Objects.equals(passport, user.passport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, passport);
+    }
+}
